48ccefd8e2f481e06f961c890dc1a2910ce6414e
asantiagode <asantiago.duque@udea.edu.co>
2024-02-29 00:36:27 -0500

    database creation

---
+++ Orders/Orders.Backend/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.Shared.Entities;
+
+//using Orders.Shared.Entities;
+
+namespace Orders.Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        // All entities that I want to map to the database need have the DbSet property
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+
+    }
+}
--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -1,11 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Orders.Backend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Inyect connection
+builder.Services.AddDbContext<DataContext>(x=>x.UseSqlServer("name=LocalConnection"));
 
 var app = builder.Build();
 

a418ccc2efa0cb70b2657d3acaadabcc43285b07
asantiagode <asantiago.duque@udea.edu.co>
2024-03-03 21:23:46 -0500

    feat: add basic countries api controller

--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -3,13 +3,12 @@ using Orders.Backend.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Inyect connection
-builder.Services.AddDbContext<DataContext>(x=>x.UseSqlServer("name=LocalConnection"));
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
 
 var app = builder.Build();
 
@@ -25,5 +24,12 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Reduce the security layer to allow the frontend to connect
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
 
 app.Run();

88700310fbe4c47a7040036966e018a1402bfbeb
asantiagode <asantiago.duque@udea.edu.co>
2024-03-05 00:25:01 -0500

    wip: Repository class

--- Orders/Orders.Frontend/Program.cs
+++ Orders/Orders.Frontend/Program.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Orders.Frontend;
 
+const int BACKEND_PORT = 7216;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri($"https://localhost:{BACKEND_PORT}") });
 
 await builder.Build().RunAsync();
---
+++ Orders/Orders.Frontend/Repositories/Repository.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+
+namespace Orders.Frontend.Repositories
+{
+    public class Repository : IRepository
+    {
+        private readonly HttpClient _httpClient;
+
+        private JsonSerializerOptions _serializerOptions => new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        public Repository(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<HttpResponseWrapper<T>> GetAsync<T>(string url)
+        {
+            var responseHttp = await _httpClient.GetAsync(url);
+            if (responseHttp.IsSuccessStatusCode)
+            {
+                var response = await UnserializeAnswer<T>(responseHttp);
+                return new HttpResponseWrapper<T>(response, false, responseHttp);
+            }
+
+            return new HttpResponseWrapper<T>(default, true, responseHttp);
+        }
+
+        public Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model)
+        { }
+
+        public Task<HttpResponseWrapper<TActionResponse>> PostAsync<T, TActionResponse>(string url, T model)
+        { }
+
+        private async Task<T> UnserializeAnswer<T>(HttpResponseMessage responseHttp)
+        {
+            var response = await responseHttp.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(response, _serializerOptions)!;
+        }
+    }
+}
